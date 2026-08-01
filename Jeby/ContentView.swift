ff1522432ea0d70a02f019955eafdcc74bdd3164
Henry Macht <hmacht88@gmail.com>
2026-08-01 10:56:14 -0400

    Initial Commit

---
+++ Jeby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jeby
+//
+//  Created by Henry Macht on 8/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jeby/JebyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JebyApp.swift
+//  Jeby
+//
+//  Created by Henry Macht on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct JebyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
